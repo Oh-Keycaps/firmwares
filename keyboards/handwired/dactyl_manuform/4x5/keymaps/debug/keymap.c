9a3a121adb6f850312c0e1b67cab81f9c3a0d05d
Robert Snyder <immeraufdemhund@hotmail.com>
2021-02-13 16:39:54 -0600

    add 4x5 keymap files

---
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/debug/keymap.c
@@ -0,0 +1,39 @@
+#include QMK_KEYBOARD_H
+#include <print.h>
+
+extern keymap_config_t keymap_config;
+
+#define _QWERTY 0
+#define _LOWER 1
+
+#define LOWER MO(_LOWER)
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_QWERTY] = LAYOUT(
+LOWER  ,LOWER  ,LOWER  ,LOWER  ,LOWER  ,                      LOWER  ,LOWER  ,LOWER  ,LOWER  ,LOWER  ,
+LOWER  ,LOWER  ,LOWER  ,LOWER  ,LOWER  ,                      LOWER  ,LOWER  ,LOWER  ,LOWER  ,LOWER  ,
+LOWER  ,LOWER  ,LOWER  ,LOWER  ,LOWER  ,                      LOWER  ,LOWER  ,LOWER  ,LOWER  ,LOWER  ,
+        LOWER  ,LOWER  ,                                                      LOWER  ,LOWER  ,
+                        LOWER  ,LOWER  ,                      LOWER  ,LOWER  ,
+                                LOWER  ,LOWER  ,      LOWER  ,LOWER  ,
+                                LOWER  ,LOWER  ,      LOWER  ,LOWER
+),
+
+[_LOWER] = LAYOUT(
+RESET  ,DEBUG  ,_______,_______,_______,                       RESET  ,DEBUG  ,_______,_______,_______,
+_______,_______,_______,_______,_______,                       _______,_______,_______,_______,_______,
+_______,_______,_______,_______,_______,                       _______,_______,_______,_______,_______,
+        _______,_______,                                                       _______,_______,
+                        _______,_______,                       _______,_______,
+                                _______,_______,       _______,_______,
+                                _______,_______,       _______,_______
+)
+};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    uint16_t code = (record->event.pressed << 7) | (record->event.key.row << 3) | record->event.key.col;
+    print_hex8(code);
+    return true;
+};
+
+void keyboard_post_init_user(void) {
+    debug_enable=true;
+};
---
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/debug/rules.mk
@@ -0,0 +1,2 @@
+CONSOLE_ENABLE = yes         # Console for debug(+400)
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight. 
