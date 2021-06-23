8357fdc062287272c8cf7a60ff83d17fcbc2b472
Derek Nheiley <derek.nheiley@gmail.com>
2021-06-22 22:33:15 -0400

    add vial support for 5x6 via12 keymap

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/via12/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/via12/config.h
@@ -1,5 +1,16 @@
 #pragma once
 
+#define VIAL_KEYBOARD_UID {0x80, 0x1F, 0xB7, 0x86, 0xB3, 0x42, 0x16, 0x92}
+
+/* top-left and top-right keys */
+#undef VIAL_UNLOCK_COMBO_ROWS
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#undef VIAL_UNLOCK_COMBO_COLS
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#undef MANUFACTURER
+#define MANUFACTURER Oh Key Caps
+
 #define USE_SERIAL
 #define EE_HANDS
 
