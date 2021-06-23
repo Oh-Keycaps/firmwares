8d4960c08d00618e94453f91ade5adddfcf2fa12
Derek Nheiley <derek.nheiley@gmail.com>
2021-06-22 23:12:14 -0400

    add vial support for 4x5, 4x6, and 6x6

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/via/config.h
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/via/config.h
@@ -18,9 +18,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define VIAL_KEYBOARD_UID {0xEA, 0x80, 0x92, 0xF3, 0x55, 0x9C, 0x48, 0xDF}
+
+/* top-left and top-right keys */
+#undef VIAL_UNLOCK_COMBO_ROWS
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#undef VIAL_UNLOCK_COMBO_COLS
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#undef MANUFACTURER
+#define MANUFACTURER Oh Key Caps
+
 #define USE_SERIAL
 #define EE_HANDS
 
+#undef RGBLED_NUM
+#define RGBLED_NUM 12
 #define RGBLED_SPLIT { 6, 6 }
 #define RGBLIGHT_ANIMATIONS
 #define FLIP_HALF
