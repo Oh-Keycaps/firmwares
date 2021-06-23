2cd5f5752afca1d2e32c46af3442dbfc0e2d4a3f
Derek Nheiley <derek.nheiley@gmail.com>
2021-06-22 22:44:27 -0400

    add vial support for 5x6 via keymap

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/via/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/via/config.h
@@ -1,7 +1,20 @@
 #pragma once
 
+#define VIAL_KEYBOARD_UID {0xE7, 0x97, 0x85, 0x3E, 0x7B, 0xB6, 0x98, 0x16}
+
+/* top-left and top-right keys */
+#undef VIAL_UNLOCK_COMBO_ROWS
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#undef VIAL_UNLOCK_COMBO_COLS
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#undef MANUFACTURER
+#define MANUFACTURER Oh Key Caps
+
 #define USE_SERIAL
 #define EE_HANDS
 
 #define RGBLED_SPLIT { 6, 6}
+#undef RGBLED_NUM
+#define RGBLED_NUM 12
 #define RGBLIGHT_ANIMATIONS
