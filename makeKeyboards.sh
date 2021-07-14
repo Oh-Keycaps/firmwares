1b51a62fb011fc7586785a36048bef9f565d5a11
Robert Snyder <rsnyder@dminc.com>
2021-07-14 12:21:34 -0400

    change copy to move

--- makeKeyboards.sh
+++ makeKeyboards.sh
@@ -13,9 +13,9 @@ bin/qmk compile -kb handwired/dactyl_promicro -km via
 bin/qmk compile -kb handwired/dactyl_promicro -km via12
 popd
 
-cp -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_4x5*.hex ./keyboards/handwired/dactyl_manuform/4x5/
-cp -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_4x6*.hex ./keyboards/handwired/dactyl_manuform/4x6/
-cp -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_5x6*.hex ./keyboards/handwired/dactyl_manuform/5x6/
-cp -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_6x6*.hex ./keyboards/handwired/dactyl_manuform/6x6/
-cp -v ../../qmk/qmk_firmware/handwired_dactyl_promicro*.hex ./keyboards/handwired/dactyl_promicro/
+mv -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_4x5*.hex ./keyboards/handwired/dactyl_manuform/4x5/
+mv -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_4x6*.hex ./keyboards/handwired/dactyl_manuform/4x6/
+mv -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_5x6*.hex ./keyboards/handwired/dactyl_manuform/5x6/
+mv -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_6x6*.hex ./keyboards/handwired/dactyl_manuform/6x6/
+mv -v ../../qmk/qmk_firmware/handwired_dactyl_promicro*.hex ./keyboards/handwired/dactyl_promicro/
 
