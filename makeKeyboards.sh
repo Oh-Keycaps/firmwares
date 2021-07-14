e842ffd13b880250cb3fcc44250beb25ee4265fb
Robert Snyder <rsnyder@dminc.com>
2021-07-14 12:26:28 -0400

    installed qmk no longer need bin/qmk

--- makeKeyboards.sh
+++ makeKeyboards.sh
@@ -1,16 +1,16 @@
 cp -r -v keyboards/ ../../qmk/qmk_firmware/
 
 pushd ../../qmk/qmk_firmware/
-bin/qmk compile -kb handwired/dactyl_manuform/4x5 -km via
-bin/qmk compile -kb handwired/dactyl_manuform/4x5 -km via12
-bin/qmk compile -kb handwired/dactyl_manuform/4x6 -km via
-bin/qmk compile -kb handwired/dactyl_manuform/4x6 -km via12
-bin/qmk compile -kb handwired/dactyl_manuform/5x6 -km via
-bin/qmk compile -kb handwired/dactyl_manuform/5x6 -km via12
-bin/qmk compile -kb handwired/dactyl_manuform/6x6 -km via
-bin/qmk compile -kb handwired/dactyl_manuform/6x6 -km via12
-bin/qmk compile -kb handwired/dactyl_promicro -km via
-bin/qmk compile -kb handwired/dactyl_promicro -km via12
+qmk compile -kb handwired/dactyl_manuform/4x5 -km via
+qmk compile -kb handwired/dactyl_manuform/4x5 -km via12
+qmk compile -kb handwired/dactyl_manuform/4x6 -km via
+qmk compile -kb handwired/dactyl_manuform/4x6 -km via12
+qmk compile -kb handwired/dactyl_manuform/5x6 -km via
+qmk compile -kb handwired/dactyl_manuform/5x6 -km via12
+qmk compile -kb handwired/dactyl_manuform/6x6 -km via
+qmk compile -kb handwired/dactyl_manuform/6x6 -km via12
+qmk compile -kb handwired/dactyl_promicro -km via
+qmk compile -kb handwired/dactyl_promicro -km via12
 popd
 
 mv -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_4x5*.hex ./keyboards/handwired/dactyl_manuform/4x5/
