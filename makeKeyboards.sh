b574ce44c4bf1ccb4f7666f923c61d84f77d9a89
Robert Snyder <rsnyder@dminc.com>
2021-07-14 21:01:18 -0400

    change to use vial directory for building qmk

--- makeKeyboards.sh
+++ makeKeyboards.sh
@@ -1,6 +1,7 @@
-cp -r -v keyboards/ ../../qmk/qmk_firmware/
+qmk=../../vial-kb/vial-qmk
+cp -r -v keyboards/ $qmk
 
-pushd ../../qmk/qmk_firmware/
+pushd $qmk
 qmk compile -kb handwired/dactyl_manuform/4x5 -km via
 qmk compile -kb handwired/dactyl_manuform/4x5 -km via12
 qmk compile -kb handwired/dactyl_manuform/4x6 -km via
@@ -13,9 +14,9 @@ qmk compile -kb handwired/dactyl_promicro -km via
 qmk compile -kb handwired/dactyl_promicro -km via12
 popd
 
-mv -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_4x5*.hex ./keyboards/handwired/dactyl_manuform/4x5/
-mv -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_4x6*.hex ./keyboards/handwired/dactyl_manuform/4x6/
-mv -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_5x6*.hex ./keyboards/handwired/dactyl_manuform/5x6/
-mv -v ../../qmk/qmk_firmware/handwired_dactyl_manuform_6x6*.hex ./keyboards/handwired/dactyl_manuform/6x6/
-mv -v ../../qmk/qmk_firmware/handwired_dactyl_promicro*.hex ./keyboards/handwired/dactyl_promicro/
+mv -v $qmk/handwired_dactyl_manuform_4x5*.hex ./keyboards/handwired/dactyl_manuform/4x5/
+mv -v $qmk/handwired_dactyl_manuform_4x6*.hex ./keyboards/handwired/dactyl_manuform/4x6/
+mv -v $qmk/handwired_dactyl_manuform_5x6*.hex ./keyboards/handwired/dactyl_manuform/5x6/
+mv -v $qmk/handwired_dactyl_manuform_6x6*.hex ./keyboards/handwired/dactyl_manuform/6x6/
+mv -v $qmk/handwired_dactyl_promicro*.hex ./keyboards/handwired/dactyl_promicro/
 
