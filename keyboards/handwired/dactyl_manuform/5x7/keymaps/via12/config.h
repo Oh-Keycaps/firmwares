cc7907d5da481d512a79106499b2b14a45e5dfc7
Robert Snyder <immeraufdemhund@hotmail.com>
2021-11-10 18:46:02 -0600

    adding support for 5x7

---
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/via12/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDB, 0x49, 0xBC, 0x2E, 0xE0, 0xFF, 0xA2, 0x4F}
+
+#undef MANUFACTURER
+#define MANUFACTURER OhKeycaps
+
+#define USE_SERIAL
+#define EE_HANDS
+
+#define RGBLED_SPLIT { 12, 12}
+#undef RGBLED_NUM
+#define RGBLED_NUM 24
+#define RGBLIGHT_ANIMATIONS
--- makeKeyboards.sh
+++ makeKeyboards.sh
@@ -1,5 +1,5 @@
 qmk=../../vial-kb/vial-qmk
-cp -r -v keyboards/ $qmk
+cp -rv keyboards/ $qmk
 
 pushd $qmk
 qmk compile -kb handwired/dactyl_manuform/4x5 -km via
@@ -8,6 +8,8 @@ qmk compile -kb handwired/dactyl_manuform/4x6 -km via
 qmk compile -kb handwired/dactyl_manuform/4x6 -km via12
 qmk compile -kb handwired/dactyl_manuform/5x6 -km via
 qmk compile -kb handwired/dactyl_manuform/5x6 -km via12
+qmk compile -kb handwired/dactyl_manuform/5x7 -km via
+qmk compile -kb handwired/dactyl_manuform/5x7 -km via12
 qmk compile -kb handwired/dactyl_manuform/6x6 -km via
 qmk compile -kb handwired/dactyl_manuform/6x6 -km via12
 qmk compile -kb handwired/dactyl_promicro -km via
@@ -17,6 +19,7 @@ popd
 mv -v $qmk/handwired_dactyl_manuform_4x5*.hex ./keyboards/handwired/dactyl_manuform/4x5/
 mv -v $qmk/handwired_dactyl_manuform_4x6*.hex ./keyboards/handwired/dactyl_manuform/4x6/
 mv -v $qmk/handwired_dactyl_manuform_5x6*.hex ./keyboards/handwired/dactyl_manuform/5x6/
+mv -v $qmk/handwired_dactyl_manuform_5x7*.hex ./keyboards/handwired/dactyl_manuform/5x7/
 mv -v $qmk/handwired_dactyl_manuform_6x6*.hex ./keyboards/handwired/dactyl_manuform/6x6/
 mv -v $qmk/handwired_dactyl_promicro*.hex ./keyboards/handwired/dactyl_promicro/
 
